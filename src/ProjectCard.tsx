ebba8bbd72d38393257fee33f438cf296d11eeca
Samuel Pitko <samuel.pitko@gmail.com>
2023-10-07 01:41:36 +0300

    add firebase hosting

--- src/ProjectCard.tsx
+++ src/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import catalogRaw from "./assets/catalog.json";
 
 const catalog = catalogRaw as CitizenScienceFeed;
@@ -7,7 +7,7 @@ const ProjectCard: React.FC<{ project: Project }> = ({ project }) => {
   return (
     <div style={{ border: "1px solid #ddd", padding: "15px", margin: "10px" }}>
       <h3>{project.project_name}</h3>
-      <p>{project.project_description}</p>
+      <ReadMore text={project.project_description} maxLength={200}></ReadMore>
       <a
         href={project.project_url_external}
         target="_blank"
@@ -28,6 +28,35 @@ const ProjectCards: React.FC = () => {
         <ProjectCard key={index} project={project} />
       ))}
     </div>
+  );
+};
+
+const ReadMore = ({ text, maxLength }: { text: string; maxLength: number }) => {
+  const [showFullText, setShowFullText] = useState(false);
+
+  if (text.length <= maxLength) {
+    return <span>{text}</span>;
+  }
+
+  const buttonStyle = {
+    fontSize: "0.75rem", // smaller font size
+    padding: "3px 5px", // reduced padding
+    margin: "0 5px", // some margin around the button
+    cursor: "pointer", // hand cursor on hover
+    border: "1px solid #ccc", // border
+    borderRadius: "3px", // rounded corners
+  };
+
+  return (
+    <span>
+      {showFullText ? text : `${text.substring(0, maxLength)}... `}
+      <button
+        onClick={() => setShowFullText(!showFullText)}
+        style={buttonStyle}
+      >
+        {showFullText ? "Read Less" : "Read More"}
+      </button>
+    </span>
   );
 };
 
---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDrTeLxLDxnfyctBl5TaD8k1yzDC_1N8wA",
+  authDomain: "odyssey-9d76e.firebaseapp.com",
+  projectId: "odyssey-9d76e",
+  storageBucket: "odyssey-9d76e.appspot.com",
+  messagingSenderId: "874042016950",
+  appId: "1:874042016950:web:037bdd7d1255013fe4d217",
+};
+
+export default initializeApp(firebaseConfig);
