0bfda42b514c92cb0b049c3516819af5f7cf98a8
Samuel Pitko <samuel.pitko@gmail.com>
2023-10-06 23:15:41 +0300

    add boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,39 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import catalogJson from "./assets/catalog.json";
 import "./App.css";
 import ProjectCards from "./ProjectCard";
-
-const catalog = catalogJson as CitizenScienceFeed;
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <section className="side">
+          <div
+            style={{
+              border: "1px solid #ddd",
+              padding: "15px",
+              margin: "10px",
+              height: "100%",
+            }}
+          ></div>
+        </section>
+        <section className="middle">
+          <ProjectCards />
+        </section>
+        <section className="side">
+          <div
+            style={{
+              border: "1px solid #ddd",
+              padding: "15px",
+              margin: "10px",
+              height: "100%",
+            }}
+          ></div>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <ProjectCards />
     </>
   );
 }
