eb4ecccfd65f0a771edb3ca1ec88f0d182a37203
Samuel Pitko <samuel.pitko@gmail.com>
2023-10-07 16:51:53 +0300

    remove right side

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,6 @@ function App() {
         <section className="middle">
           <ProjectCards />
         </section>
-        <section className="side">
-          <div
-            style={{
-              border: "1px solid #ddd",
-              padding: "15px",
-              margin: "10px",
-              height: "100%",
-            }}
-          ></div>
-        </section>
       </div>
     </>
   );
