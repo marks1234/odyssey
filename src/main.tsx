eb0e74e030dd27b32803f948211ed494dcc25392
Samuel Pitko <samuel.pitko@gmail.com>
2023-10-07 16:49:14 +0300

    add dark mode toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import ProjectPage from "./ProjectPage.tsx";
+import { ThemeProvider, useTheme } from "./ThemeContext.tsx";
+import NavBar from "./marco/navbar/navbar.tsx";
 import Login from "./mauno/login.tsx";
 import Register from "./mauno/register.tsx";
-import ProjectPage from "./ProjectPage.tsx";
-import NavBar from "./marco/navbar/navbar.tsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -26,10 +27,31 @@ const router = createBrowserRouter([
   },
 ]);
 
+function useThemeUpdate() {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (root) {
+      root.setAttribute("data-bs-theme", theme);
+    }
+  }, [theme]);
+}
+
+function Main() {
+  useThemeUpdate();
+  return (
+    <>
+      <NavBar />
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NavBar />
-    <RouterProvider router={router} />
-
+    <ThemeProvider>
+      <Main />
+    </ThemeProvider>
   </React.StrictMode>
 );
--- src/marco/navbar/navbar.tsx
+++ src/marco/navbar/navbar.tsx
@@ -1,52 +1,61 @@
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-import { Navbar, Button, Nav, Offcanvas } from "react-bootstrap";
-import Container from 'react-bootstrap/Container';
+import { Button, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import { useTheme } from "../../ThemeContext";
 import "./navbar.css";
-import { Link } from "react-router-dom";
 
-function NavBar() {
-	const [show, setShow] = useState(false);
+function NavBar({}: {}) {
+  const [show, setShow] = useState(false);
 
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-	return (
-		<>
-			<Navbar className='nav' expand='lg'>
-				<Container>
-					<Nav.Link onClick={handleShow}>
-						{/* This is a basic 3-line "hamburger" icon. You can replace with an icon component or image */}
-						&#9776;
-					</Nav.Link>
-					<Navbar.Brand className='mx-auto'>
-						<Nav.Link href="/" >
-						odyssey
-						</Nav.Link>						
-					</Navbar.Brand>
+  const { toggleTheme, theme } = useTheme();
 
-					{/* <Nav.Link href="/" className='mx-auto'>odyssey</Nav.Link> */}
-				<Nav>
-					<Nav.Link href="/login">login</Nav.Link>
-					<Nav.Link href="/register">signup</Nav.Link>
-				</Nav>
-				</Container>
-			</Navbar>
+  return (
+    <>
+      <Navbar className="nav" expand="lg">
+        <Container>
+          <Nav.Link onClick={handleShow}>
+            {/* This is a basic 3-line "hamburger" icon. You can replace with an icon component or image */}
+            &#9776;
+          </Nav.Link>
+          <Navbar.Brand className="mx-auto">
+            <Nav.Link href="/">odyssey</Nav.Link>
+          </Navbar.Brand>
 
-			<Offcanvas show={show} onHide={handleClose} placement='start'>
-				<Offcanvas.Header closeButton>
-					<Offcanvas.Title>Offcanvas</Offcanvas.Title>
-				</Offcanvas.Header>
-				<Offcanvas.Body>
-					{/* Contents similar to GitHub's slide-out window can be placed here */}
-					{/* Your content here */}
-					<p>User page</p>
-					<p>Projects</p>
-					<p>Resources</p>
-					<p>API</p>
-				</Offcanvas.Body>
-			</Offcanvas>
-		</>
-	);
+          {/* <Nav.Link href="/" className='mx-auto'>odyssey</Nav.Link> */}
+          <Nav>
+            <Nav.Link href="/login">login</Nav.Link>
+            <Nav.Link href="/register">signup</Nav.Link>
+            <Button onClick={toggleTheme} variant="outline-primary">
+              {theme === "light" ? (
+                <FontAwesomeIcon icon={faMoon} />
+              ) : (
+                <FontAwesomeIcon icon={faSun} />
+              )}
+            </Button>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Offcanvas show={show} onHide={handleClose} placement="start">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          {/* Contents similar to GitHub's slide-out window can be placed here */}
+          {/* Your content here */}
+          <p>User page</p>
+          <p>Projects</p>
+          <p>Resources</p>
+          <p>API</p>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
 }
 
 export default NavBar;
