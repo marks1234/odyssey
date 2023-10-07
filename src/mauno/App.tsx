eb7a9c067deaae808889de1b1c35d3c5ba8bce52
beansuu <mauno.talli@hotmail.com>
2023-10-07 07:13:16 +0300

    Login and registration forms

---
+++ src/mauno/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+//import Login from "./mauno/login";
+import Register from "./register";
+
+
+function App() {
+ // return <Login/>
+  return <Register/>
+}
+export default App;
